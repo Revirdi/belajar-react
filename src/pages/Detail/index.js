4aa3c9b182aa9718d5ae8f6042884416535c77b8
Revirdi <ardwiyanrevirdi99@gmail.com>
2022-06-14 14:51:40 +0700

    anjing

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,28 +1,56 @@
+// import axios from "axios";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axiosInstance from "../../services/axios";
 
 function DetailProduct() {
   // params.productId --> gunakan untuk ambil data
+  const [filteredProduct, setfilteredProduct] = useState([]);
+  const [quantity, setquantity] = useState(0);
   const params = useParams();
+
+  const increment = () => {
+    return setquantity(quantity + 1);
+  };
+  const decrement = () => {
+    if (quantity > 0) return setquantity(quantity - 1);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const resGetProducts = await axiosInstance.get(
+        `/products/${params.productId}`
+      );
+      setfilteredProduct(resGetProducts.data);
+    } catch (error) {
+      alert("Terjadi kesalahan. Api udah dinyalain ?");
+      console.log({ error });
+    }
+  };
+
+  const { productName, price, productImage, description } = filteredProduct;
   return (
     <div className="container">
       <div className="row mt-3">
         <div className="col-6">
-          <img
-            style={{ width: "100%" }}
-            src={
-              "https://d29c1z66frfv6c.cloudfront.net/pub/media/catalog/product/large/35986529ca3dc88d58695fe1396f909aaf0eea57_xxl-1.jpg"
-            }
-            alt="tulisan"
-          />
+          <img style={{ width: "100%" }} src={productImage} alt="tulisan" />
         </div>
         <div className="col-6 d-flex flex-column justify-content-center">
-          <h4>Kaos Baja Hitam</h4>
-          <h5>Rp 20.000</h5>
-          <p>Kaos baja hitam pembela kebenaran</p>
+          <h4>{productName}</h4>
+          <h5>{price}</h5>
+          <p>{description}</p>
           <div className="d-flex flex-row align-items-center">
-            <button className="btn btn-primary ">-</button>
-            <strong className="text-center mx-4">30</strong>
-            <button className="btn btn-primary ">+</button>
+            <button className="btn btn-primary " onClick={decrement}>
+              -
+            </button>
+            <strong className="text-center mx-4">{quantity}</strong>
+            <button className="btn btn-primary " onClick={increment}>
+              +
+            </button>
           </div>
           <button className="btn btn-success mt-3">Add to cart</button>
         </div>
